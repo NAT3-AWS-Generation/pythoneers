a047557de1684da268aae39a10fef84873404ba6
Fran-T6488 <franciscot@genstudents.org>
2024-09-18 11:40:14 +0100

    add test.py

---
+++ application-project/prompts/test.py
@@ -0,0 +1,68 @@
+
+# Book shop and Cafe
+# Dictionarys
+food {
+    "Coffee" : "£2.00"
+}
+
+drinks {
+
+}
+
+books {
+    
+}
+
+order = ["Coffee", "Book_1"]
+
+customers = []
+
+class Customer:
+ def __init__(self, name, address) -> None:
+  self.name = name
+  self.address = address
+  self.order = []
+
+  def get_customer_info(self):
+   asdas
+
+john = Customer("John", "1223 Fake street")
+
+customers = [john, etc]
+
+# def functions
+# This updates book details
+# update_book()
+# add_book()
+# add_special()
+# get_customer_info()
+
+
+# Open
+# Welcome to the book cafe:
+
+#1. See our menu
+#2. see our book collection
+#3. Staff section
+#4. Exit
+
+
+if they pressed 1:
+ do this
+elif they pressed 2:
+ do this
+
+# Menu
+# 1. Foods
+# 2. Drinks
+
+
+# Food
+# All the foods
+#1. Croissant
+#
+#
+#
+#5. done
+
+order.append(Whatever they ordered)
